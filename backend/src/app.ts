3caffdff48bd925c283ae24f86fa26e9c27d095f
oday <odaysaad2003@gmail.com>
2026-06-28 22:14:06 +0300

    add bcrypt and jwt

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -25,6 +25,7 @@ app.use(express.json({limit: "1mb"}));
 app.use(express.urlencoded({extended: true, limit: "1mb"}));
 
 app.use("/api/v1/health", healthRouter);
+// test api
 app.use("/api/v1/system/status", routersystem);
 
 app.use((_req, res) => {
